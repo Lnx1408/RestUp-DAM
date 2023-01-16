5ff48aa0db29037ad9e7e810bb774fe946a164f5
Barbara <ar_dayana@outlook.es>
2023-01-15 22:46:18 -0500

    Form de cambio de contraseña(cliente).

---
+++ app/src/main/java/com/aply/taller/ModiPassword.java
@@ -0,0 +1,14 @@
+package com.aply.taller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModiPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modi_password);
+    }
+}
